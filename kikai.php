f1c798ca10d6f10e8808510f84461e32c393341e
fs-designer <orange32343234@gmail.com>
2024-08-01 16:07:45 +0900

    Add files via upload

---
+++ kikai.php
@@ -0,0 +1,33 @@
+<section id="top" class="page">
+  <div class="common-hero">
+    <div class="common-hero__inner s2">
+      <div class="text-box">
+        <p class="name">Technology</p>
+        <p class="title">機械加工事業</p>
+        <p class="text">極小部品から大型部品までをトータルに。<br>精密機械加工から企画設計組み立てまでの一貫生産が可能です。</p>
+      </div>
+    </div>
+    <div class="scroll-line"></div>
+    <p class="scroll-text x-center">scroll</p>
+  </div>
+  <div class="anime-block"></div>
+</section>
+<section id="seimitsu">
+  <div class="common-catch-text-outter">
+    <h3 class="common-catch-text slide-left-soon">グローバルな生産体制による一貫生産。<br class="sp">お客様のベストパートナーであり続ける。<br class="sp">それが私たちの仕事です。</h3>
+  </div>
+  <div class="cnc">
+    <div class="wrap info-outter">
+      <div class="info scrollanime downup">
+        <h3 class="common-title">CNC工作機械 計100台以上保有</h3>
+        <div class="info-group x-center">
+          <p class="text">本社工場・長野駒ヶ根工場・中国大連豊実有限公司の3工場に、<br>最新コンピューター数値制御システムが導入されたCNC工作機械を計100台以上保有。<br>
+          旋削・切削・研削加工までを範疇とする幅広い対応はもちろん、設計から組み立てに至る<br>一貫生産も得意とし、極小部品から小ロットの大型部品加工をすべて自社にて手掛ける生産体制で、<br>お客様のあらゆるご要望に柔軟に対応いたします。<br>
+          また、中国大連の自社工場をはじめ、海外工場とのタイアップによるグローバル展開を進めることで<br>低コスト化を図り、自社一貫生産管理システムによって、受注から納品までのリードタイムの短縮にも努めます。</p>
+        </div>
+      </div>
+      [works term_slug='seimitsu']
+    </div>
+  </div>
+  [facility]
+</section>
